7abd83d94986c48d0a0832aaa7c863a96f18107f
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-12-31 14:02:40 -0600

    Add patch to workaround static TLS block issue
    https://github.com/apache/incubator-mxnet/issues/8976
    https://github.com/jemalloc/jemalloc/issues/937

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,9 @@ if [[ ${HOST} =~ .*darwin.* ]] ; then
   export LDFLAGS="${LDFLAGS_CC} -Wl,-rpath,${PREFIX}/lib"
 fi
 
-./configure --prefix=$PREFIX \
-            --build=${BUILD} \
-            --host=${HOST}
+./autogen.sh --prefix=$PREFIX \
+             --build=${BUILD} \
+             --host=${HOST}
 make -j${CPU_COUNT}
 make tests
 make check
