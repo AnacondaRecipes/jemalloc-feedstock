adfdfe59ffef89b0290060c8490bb069a11c8c30
Ray Donnelly <mingw.android@gmail.com>
2018-01-04 21:27:56 +0000

    Removal of LDFLAGS_CC on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,10 @@
 set -e
 set -x
 
-if [[ ${HOST} =~ .*darwin.* ]] ; then
-  export LDFLAGS="${LDFLAGS_CC} -Wl,-rpath,${PREFIX}/lib"
-fi
-
 ./autogen.sh --prefix=$PREFIX \
              --build=${BUILD} \
              --host=${HOST}
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make tests
 make check
 make install
